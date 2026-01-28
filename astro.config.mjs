dbe12a407092ecdb0427e9ddb6d65b87b210827d
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2026-01-28 15:58:02 -0500

    pls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site : "https://crilillo.me",
-  base : "/",
+  base : "/crilillo",
   output: 'static',
 
   vite: {
